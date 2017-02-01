d308694f8638c0323855eeb2ece9aeb7da1055e2
Chris Arcand <chris@chrisarcand.com>
2017-02-01 10:05:25 -0500

    There can be only one!

--- lib/highlander.rb
+++ lib/highlander.rb
@@ -10,7 +10,7 @@ BEGIN{
     status = DATA.flock(File::LOCK_EX | File::LOCK_NB)
 
     if status != 0
-      raise RuntimeError, "Program '#{program}' already running"
+      raise RuntimeError, "There can be only one! Program '#{program}' already running"
       exit! # In case of rescue, forcibly bail
     end
   end
